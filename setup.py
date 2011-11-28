6f6c798a388f958a857b3df47e34eff97680237e
Nikita Hismatov <hismatovns@gmail.com>
2011-11-28 11:59:15 +0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='authplug',
+    version='0.1',
+    description='Authplug - pluggable auth',
+    long_description='An authentication application for django',
+    author='nskeip',
+    author_email='me@ns-keip.ru',
+    url='https://github.com/nskeip/authplug',
+    packages=['authplug'],
+    requires=['django']
+)
