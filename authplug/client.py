605d5a3c12bcf05c0de8d7455eff7a641d06e1c2
Nikita Hismatov <hismatovns@gmail.com>
2011-11-29 13:51:04 +0600

    убрал русские комменты и переименовал второй хэш

--- authplug/client.py
+++ authplug/client.py
@@ -9,10 +9,9 @@ def sign(params, salt, date=None):
     hash = u'&'.join([u'='.join((unicode(k), unicode(v))) for k, v in items])
     date = date or datetime2str(datetime.utcnow())
 
-    #прибаляем дату в формате UTC2 и применяем sha1
     s1 = u''.join((hash, date))
     hash = hashlib.sha1(s1).hexdigest()
-    #добавляем ключ и применяем sha1
-    hash = hashlib.sha1(u''.join((hash, salt))).hexdigest()
 
-    return unicode(hash)
+    salted_hash = hashlib.sha1(u''.join((hash, salt))).hexdigest()
+
+    return unicode(salted_hash)
